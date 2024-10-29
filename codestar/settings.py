79a787a58242e70a5dc31f813bc993ff3fe97f49
mezter1337 <andreas.javert@gmail.com>
2024-10-29 15:18:07 +0100

    add comment model

--- codestar/settings.py
+++ codestar/settings.py
@@ -32,7 +32,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".herokuapp.com"]
 
-CSRF_TRUSTED_ORIGINS = ["https://*.codeinstitute-ide.net/", "https://*.herokuapp.com"]
+CSRF_TRUSTED_ORIGINS = ["https://127.0.0.1", "https://*.herokuapp.com"]
 
 
 # Application definition
