668e4aa483609d9b4e88b43a318630f4a0944888
mezter1337 <andreas.javert@gmail.com>
2024-11-01 22:52:01 +0100

    add collaboration request and a POST handler

--- about/admin.py
+++ about/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import About
+from .models import About, CollaborateRequest
 from django_summernote.admin import SummernoteModelAdmin
 
 
@@ -7,3 +7,11 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(About)
 class AboutAdmin(SummernoteModelAdmin):
     summernote_fields = ("content",)
+
+
+@admin.register(CollaborateRequest)
+class CollaborateRequestAdmin(SummernoteModelAdmin):
+    list_display = (
+        "name",
+        "read",
+    )
--- about/views.py
+++ about/views.py
@@ -1,13 +1,32 @@
-from django.shortcuts import render
-from .models import About
+from django.shortcuts import render, get_object_or_404
+from .models import About, CollaborateRequest
+from .forms import CollaborateForm
+from django.contrib import messages
 
 
 # Create your views here.
 def about_me(request):
+
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form = collaborate_form.save(commit=False)
+            collaborate_form.save()
+
+            messages.add_message(
+                request,
+                messages.SUCCESS,
+                "Collaboration request received! I endeavour to respond within 2 working days.",
+            )
+
     about = About.objects.all().order_by("-updated_on").first()
-    
+    collaborate_form = CollaborateForm()
+
     return render(
         request,
         "about/about.html",
-        {"about": about},
+        {
+            "about": about,
+            "collaborate_form": collaborate_form,
+        },
     )
